3996795e13254a2cc6b8d5599d6406a265b53f1c
aadhikeaswar <117028089+aadhikeaswar@users.noreply.github.com>
2022-10-30 16:24:50 +0000

    Committing files

---
+++ CodeForces/GNU C++17/1646A | Square Counting/148323330.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long t, n, s;
+    cin >> t;
+    while(t--){
+        cin >> n >> s;
+        cout << s/(n*n) << endl;
+    }
+    return 0;
+}
