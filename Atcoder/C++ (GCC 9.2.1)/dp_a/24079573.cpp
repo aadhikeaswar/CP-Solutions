47ee03d149e586b621fa8a400a5e4784754cffb8
aadhikeaswar <117028089+aadhikeaswar@users.noreply.github.com>
2022-10-30 16:57:34 +0000

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_a/24079573.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int a[n], dp[n+1];
+    for(int &i : a) cin >> i;
+    dp[n] = 0, dp[n-1] = 0;
+    for(int i = n-2; i >= 0; i--) dp[i] = min(abs(a[i] - a[i+1]) + dp[i+1], abs(a[i] - a[i+2]) + dp[i+2]);
+    cout << dp[0];
+    return 0;
+}
