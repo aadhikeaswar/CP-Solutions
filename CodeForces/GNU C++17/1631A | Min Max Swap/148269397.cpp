4f9951c5cb70119a15a67a11152531271307e416
aadhikeaswar <117028089+aadhikeaswar@users.noreply.github.com>
2022-10-30 16:24:52 +0000

    Committing files

---
+++ CodeForces/GNU C++17/1631A | Min Max Swap/148269397.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t, n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        int a[n], b[n], mxa{0}, mxb{0};
+        for(int &i : a) cin >> i;
+        for(int &i : b) cin >> i;
+        for(int i = 0; i < n; i++) if(b[i] < a[i]) swap(a[i], b[i]);
+        for(int i = 0; i < n; i++) mxa = max(a[i], mxa), mxb = max(b[i], mxb);
+        cout << mxa*mxb << endl;
+    }
+    return 0;
+}
