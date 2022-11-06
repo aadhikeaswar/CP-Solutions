e4e3d4e62efd0470fd686a23956e5e68ff90214a
aadhikeaswar <117028089+aadhikeaswar@users.noreply.github.com>
2022-11-06 12:01:56 +0000

    Committing files

---
+++ CodeChef/C++14/TILINGS/41725658.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int dp[1000001], g[999999];
+int main() {
+    int n;
+    cin >> n;
+    dp[1] = 1, dp[2] = 2, dp[3] = 5, g[1] = 1;
+    if(n<=3) cout << dp[n];
+    else {
+        for(int i = 4; i <= n; i++)g[i-2] = (g[i-3] + dp[i-3]) % 10000, dp[i] = (dp[i-1] + dp[i-2] + 2*g[i-2])%10000;
+        cout << dp[n];
+    }
+    return 0;
+}
